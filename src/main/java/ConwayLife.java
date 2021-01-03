c53111281fe39348ab88bed4eceb9d16f583e2b1
Olga <olgazawislak@gmail.com>
2021-01-03 20:10:24 +0100

    Add fixes to ConwayLife exercise

--- src/main/java/ConwayLife.java
+++ src/main/java/ConwayLife.java
@@ -1,6 +1,5 @@
 public class ConwayLife {
     /**
-     *
      * @param cells 2d array - initial state of the game
      * @param generations number of generations in the game
      * @return final state of Conway Life universe
@@ -14,12 +13,12 @@ public class ConwayLife {
         int cellsRows = cells.length;
         int cellsColumns = cells[0].length;
 
-        int[][] cellsAfterGeneration = new int[cellsRows + 1][cellsColumns + 1];
+        int[][] cellsAfterGeneration = new int[cellsRows][cellsColumns];
 
         for (int row = 0; row < cellsRows; row++) {
             for (int col = 0; col < cellsColumns; col++) {
-                if (numberOfNeighbors(cells, row, col) == 3 ||
-                        (numberOfNeighbors(cells, row, col) == 2 && cells[row][col] == 1)) {
+                int sumOfNeighbors = numberOfNeighbors(cells, row, col);
+                if (sumOfNeighbors == 3 || (sumOfNeighbors == 2 && cells[row][col] == 1)) {
                     cellsAfterGeneration[row][col] = 1;
                 } else {
                     cellsAfterGeneration[row][col] = 0;
@@ -32,17 +31,17 @@ public class ConwayLife {
     public int numberOfNeighbors(int[][] array, int row, int column) {
         int startRow = Math.max(row - 1, 0);
         int startColumn = Math.max(column - 1, 0);
-        int endRow = Math.min(row + 1, array.length);
-        int endColumn = Math.min(column + 1, array[0].length);
-        int numberOfNeighbors = 0;
+        int endRow = Math.min(row + 1, array.length - 1);
+        int endColumn = Math.min(column + 1, array[0].length - 1);
+        int sumOfNeighbors = 0;
 
         for (int neighRow = startRow; neighRow <= endRow; neighRow++) {
             for (int neighColumn = startColumn; neighColumn <= endColumn; neighColumn++) {
-                numberOfNeighbors += array[neighRow][neighColumn];
+                if (!(neighRow == row && neighColumn == column)) {
+                    sumOfNeighbors += array[neighRow][neighColumn];
+                }
             }
         }
-        return numberOfNeighbors;
-
+        return sumOfNeighbors;
     }
-
 }

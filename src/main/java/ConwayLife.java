25fac07661900071b486d6ede4f270454ce0bce1
Olga <olgazawislak@gmail.com>
2021-01-09 22:37:50 +0100

    add tests to ConwayLife methods and fix ConwayLife and Point classes

--- src/main/java/ConwayLife.java
+++ src/main/java/ConwayLife.java
@@ -3,8 +3,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ConwayLife {
+
     /**
-     * @param cells 2d array - initial state of the game
+     * @param cells       2d array - initial state of the game
      * @param generations number of generations in the game
      * @return final state of Conway Life universe
      */
@@ -36,11 +37,10 @@ public class ConwayLife {
         for (int row = 0; row < cells.length; row++) {
             for (int col = 0; col < cells[0].length; col++) {
                 if (cells[row][col] == 1) {
-                    cellsMap.put(new Point(row, col), 0);
+                    cellsMap.put(new Point(row, col), 1);
                 }
             }
         }
-//        IntStream stream = range(0, cells.length).flatMap(row -> range(0, cells[row].length).filter();
         return cellsMap;
     }
 
@@ -49,35 +49,43 @@ public class ConwayLife {
 
         for (int i = point.getX() - 1; i <= point.getX() + 1; i++) {
             for (int j = point.getY() - 1; j <= point.getY() + 1; j++) {
-                if (pointsMap.containsKey(new Point(i, j))) {
-                    ++neighCounter;
-                }
+                neighCounter += pointsMap.getOrDefault(new Point(i, j), 0);
             }
         }
-        return neighCounter;
+        return neighCounter - pointsMap.getOrDefault(point, 0);
     }
 
-    public int[][] mapTo2dArray(Map<Point, Integer> pointsMap) {
+    public int numberOfRows(Map<Point, Integer> pointsMap) {
         int rows = 0;
-        int cols = 0;
 
         for (Point point : pointsMap.keySet()) {
             if (point.getX() > rows) {
                 rows = point.getX();
             }
+        }
+        return rows + 1;
+    }
+
+    public int numberOfColumns(Map<Point, Integer> pointsMap) {
+        int cols = 0;
+
+        for (Point point : pointsMap.keySet()) {
             if (point.getY() > cols) {
                 cols = point.getY();
             }
         }
+        return cols + 1;
+    }
+
+    public int[][] mapTo2dArray(Map<Point, Integer> pointsMap) {
+        int rows = numberOfRows(pointsMap);
+        int cols = numberOfColumns(pointsMap);
+
 
         int[][] cells = new int[rows][cols];
-        for (int row = 0; row <= rows; row++) {
-            for (int col = 0; col <= cols; col++) {
-                Point point = new Point(row, col);
-                if (pointsMap.containsKey(point)) {
-                    cells[row][col] = 1;
-                }
-                cells[row][col] = 0;
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                cells[row][col] = pointsMap.getOrDefault(new Point(row, col), 0);
             }
         }
         return cells;
--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Point {
     public int x;
     public int y;
@@ -11,15 +13,23 @@ public class Point {
         return x;
     }
 
-    public void setX(int x) {
-        this.x = x;
-    }
-
     public int getY() {
         return y;
     }
 
-    public void setY(int y) {
-        this.y = y;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Point point = (Point) o;
+        return x == point.x &&
+                y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
